0ba61cefdc361e337e40e38e62ef3b974cfd66bc
Leonardo Freire <leonardofreire.russi@icloud.com>
2022-05-27 09:24:57 -0300

    highlight style done

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components'
+import { 
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading';
+
+
+import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/Global/styles/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if(!fontsLoaded) {
+    return (<AppLoading />)
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { Feather } from '@expo/vector-icons'
+import { AnyStyledComponent } from 'styled-components'
+import Constants from 'expo-constants'
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${({theme}) => theme.colors.background};
+`
+
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme}) => theme.colors.primary};
+    align-items: center;
+`
+
+export const UserWrapper = styled.View`
+    width: 100%;
+    padding: 0 24px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${RFValue(Constants.statusBarHeight)}px;
+`
+
+export const UserInfo= styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+export const Photo= styled.Image`
+    width: ${RFValue(55)}px;
+    height: ${RFValue(55)}px;
+    border-radius: 10px;
+`
+
+export const User= styled.View`
+    margin-left: 17px;
+`
+
+export const UserGreeting= styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+
+    font-family: ${({theme}) => theme.fonts.regular};
+`
+
+export const UserName = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(18)}px;
+    
+    font-family: ${({theme}) => theme.fonts.bold};
+`
+
+export const Icon = styled(Feather as unknown as AnyStyledComponent)`
+    color: ${({theme}) => theme.colors.secondary};
+    font-size: ${RFValue(24)}px;
+`
+
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: {paddingLeft: 24}
+})`
+    width: 100%;
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`
